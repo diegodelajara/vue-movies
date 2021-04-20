9df395eca4e2fa1846dab1bfb428a4d0ced70dfa
Diego De la Jara <diego.delajara@apiservice.cl>
2021-04-20 15:19:58 -0400

    :sparkles: vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
+
+import Login from './pages/Login';
+import Home from './pages/Home'
+
+
+Vue.use(VueRouter);
 Vue.config.productionTip = false
+
+
+const router = new VueRouter({
+  routes: [
+     {
+        path: '/',
+        component: Login,
+     },
+     {
+        path: '/home',
+        component: Home,
+     }
+  ],
+});
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,38 +1,70 @@
 <template>
    <v-app id="inspire">
-      <v-content>
+      <v-main>
          <v-container fluid fill-height>
             <v-layout align-center justify-center>
                <v-flex xs12 sm8 md4>
                   <v-card class="elevation-12">
                      <v-toolbar dark color="primary">
-                        <v-toolbar-title>Login form</v-toolbar-title>
+                        <v-toolbar-title>Login</v-toolbar-title>
                      </v-toolbar>
                      <v-card-text>
                         <v-form>
                            <v-text-field
-                              prepend-icon="person"
                               name="login"
                               label="Login"
                               type="text"
+                              v-model="username"
                            ></v-text-field>
+
                            <v-text-field
                               id="password"
-                              prepend-icon="lock"
                               name="password"
                               label="Password"
                               type="password"
+                              v-model="pass"
                            ></v-text-field>
                         </v-form>
+                        <p v-if="errors.length">
+                           <b>Por favor, corrija el(los) siguiente(s) error(es):</b>
+                           <ul>
+                              <li v-for="(error, key) in errors" :key="key">{{ error }}</li>
+                           </ul>
+                        </p>
                      </v-card-text>
                      <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="primary" to="/">Login</v-btn>
+                        <v-btn color="primary" @click="onLogin">Login</v-btn>
                      </v-card-actions>
                   </v-card>
                </v-flex>
             </v-layout>
          </v-container>
-      </v-content>
+      </v-main>
    </v-app>
 </template>
+
+<script>
+export default {
+   data() {
+      return {
+         errors: [],
+         username: 'agilesoft',
+         pass: 'agile1234'
+      }
+   },
+   methods: {
+      onLogin(e) {
+         e.preventDefault();
+         console.log(this.username);
+         console.log(this.pass);
+         if (!this.name) {
+            this.errors.push('El nombre es obligatorio.');
+         }
+         if (!this.age) {
+            this.errors.push('La edad es obligatoria.');
+         }
+      }
+   }
+}
+</script>
